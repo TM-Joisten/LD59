991be70cd34d26cb74240ef7545e646df1e73740
pdues <pittduester@web.de>
2026-04-18 14:34:25 +0200

    jumping of signal

--- ld-59-game/satellite.gd
+++ ld-59-game/satellite.gd
@@ -2,17 +2,18 @@ extends Node2D
 
 
 var F = -1
-var R = 300
+#var R = 300
 var PHI1 = 0
 var T = float(Time.get_ticks_msec())/1e3
 
 @onready var Planet = $/root/main/Planet as Node2D
+@export var R: int = 300
 
 func _ready() -> void:
 	pass
 	
 func _process(delta: float) -> void:
 	T = float(Time.get_ticks_msec())/1e3
-	print(self.position)
+	#print(self.position)
 	self.global_position.x = Planet.global_position.x + R*cos(F*T + PHI1)
 	self.global_position.y = Planet.global_position.y + R*sin(F*T + PHI1)
