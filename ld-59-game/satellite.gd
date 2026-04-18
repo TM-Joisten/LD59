4e4d43c5e221361191902fecbfa2d22a26b7ee5e
BenediktPeter <benedikt.peter.zapf@gmail.com>
2026-04-18 10:33:50 +0200

    area2d add

--- ld-59-game/satellite.gd
+++ ld-59-game/satellite.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
 
-var F = -1
-var R = 300
-var PHI1 = 0
+@export var F = -6
+@export var R = 300
+@export var PHI1 = 0
+@export var HAx = 1
+@export var HAy = 1
 var T = float(Time.get_ticks_msec())/1e3
 
 @onready var Planet = $/root/main/Planet as Node2D
+@onready var Area = $Area2D
 
 func _ready() -> void:
 	pass
@@ -14,5 +17,5 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	T = float(Time.get_ticks_msec())/1e3
 	print(self.position)
-	self.global_position.x = Planet.global_position.x + R*cos(F*T + PHI1)
-	self.global_position.y = Planet.global_position.y + R*sin(F*T + PHI1)
+	self.global_position.x = Planet.global_position.x + HAx*R*cos(F*T + PHI1)
+	self.global_position.y = Planet.global_position.y + HAy*R*sin(F*T + PHI1)
