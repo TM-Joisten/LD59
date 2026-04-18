9816075689ebaf8d7b1a2c1f80c2fb0f76d1ed52
BenediktPeter <benedikt.peter.zapf@gmail.com>
2026-04-18 18:07:16 +0200

    Rangeindicator

--- ld-59-game/rangeindicator.gd
+++ ld-59-game/rangeindicator.gd
@@ -1,13 +1,24 @@
 extends Node2D
 
-@onready var parent = self.get_parent
-@onready var sprite = $AnimatedSprite2Ds
+@export var parent = Node2D
+@onready var sprite = $AnimatedSprite2D
 @export var goal = Node2D
-var animations = sprite.sprite_frames.get_animation_names()
+@onready var animations = sprite.sprite_frames.get_animation_names()
 
 func get_dist():
-	abs(parent.position - goal.position)
-	return 
-
+	var distx = abs(goal.global_position.x - parent.global_position.x)
+	var disty = abs(goal.global_position.y - parent.global_position.y)
+	var dist = abs(distx+disty)
+	return dist
 func _ready() -> void:
-	$AnimatedSprite2D.play(animations[0])
+	$AnimatedSprite2D.play(animations[1])
+
+func _process(delta: float) -> void:
+	var dist = get_dist()
+	print(dist             )
+	if dist > 750:
+		$AnimatedSprite2D.play(animations[1])
+	if dist < 750 && dist >300:
+		$AnimatedSprite2D.play(animations[2])
+	if dist < 300:
+		$AnimatedSprite2D.play(animations[0])
