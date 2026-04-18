4e1a7d9b13bd2f062529c5fd0da7fbf4546b2751
BenediktPeter <benedikt.peter.zapf@gmail.com>
2026-04-18 23:58:39 +0200

    UI

---
+++ ld-59-game/script/ui.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@export var counter = 100
+var X = 2.5 # Ticks/s
+var delta_time = 0
+@onready var Textbox = $TextEdit
+@onready var sprite = $AnimatedSprite2D
+@onready var animations = sprite.sprite_frames.get_animation_names()
+
+func _process(delta: float) -> void:
+	delta_time += delta
+	if delta_time >= 1.0 and  counter !=0 and counter <=100:
+		Textbox.clear()
+		delta_time = 0
+		counter -= X
+		Textbox.insert_text("\n " + str(int(counter)) + "%",0,0,true,false) 
+		if counter < 75 && counter > 25:
+			sprite.play("medium")
+		elif counter <= 25:
+			sprite.play("sad")
+		else: 
+			sprite.play("happy")
+	 
+	
+
+
+func _on_receiver_reach_goal() -> void:
+	counter +=10
+	pass # Replace with function body.
