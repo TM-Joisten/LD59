aa1dcd38882af510e41baaef416194a4db426b58
BenediktPeter <benedikt.peter.zapf@gmail.com>
2026-04-19 10:17:46 +0200

    weiters UI design

--- ld-59-game/script/gameover.gd
+++ ld-59-game/script/gameover.gd
@@ -1,8 +1,10 @@
 extends Control
 @export var Nhouses = 0
 @onready var subtext = $subtext
+@onready var station = $station
 @onready var button = $Button
 
 func _ready() -> void:
 	subtext.clear()
 	subtext.insert_text(" \n you reached \n  " + str(Nhouses) +" houses ",0,0,true,false)
+	$station/AnimatedSprite2D.play("signal_on")
